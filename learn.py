e423dc03fc7e4d2dbaf02ce8924701b7079e6bc4
PanMaczeta <52625108+PanMaczeta@users.noreply.github.com>
2023-10-27 15:26:41 +0200

    Added New metrics Metrics visualization Saving model Json input and training data integration Prediction script

--- learn.py
+++ learn.py
@@ -1,17 +1,22 @@
-import configparser 
+import configparser
+import os
+
 import tensorflow as tf
 
-from tools import load_data, preprocess
+from tools import load_data, preprocess, perplexity, plot_and_save_metric
+
+# Preprocessing data phase
+
 
 data_path = ''
-raw_data = load_data(data_path) # load raw data from path given
+raw_data = load_data(data_path) # load raw data from path given (TODO: SQL integration)
 
 preprocessed_data = [preprocess(qa) for qa in raw_data.split('\n')]
 
-#Params from config
+# Params from config
 training_size = len(preprocessed_data)
 config = configparser.ConfigParser()
-config.read('example.ini')
+config.read('config.ini')
 
 vectorizer = tf.keras.layers.TextVectorization(max_tokens=config["DEFAULT"]["VocabSize"], output_mode='int')
 vectorizer.adapt(preprocessed_data)
@@ -23,28 +28,32 @@ padded_sequences = tf.keras.utils.pad_sequences(sequences,
               padding=config['DEFAULT']['PaddingType'], 
               truncating=config['DEFAULT']['TruncType'])
 
-training_data = padded_sequences[:training_size]
-training_labels = padded_sequences[:training_size]
+training_data = padded_sequences[:training_size // 2]       # Typical QA dataset
+training_labels = padded_sequences[training_size // 2:]
+
+# Architecture phase
 
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(
               config['DEFAULT']['VocabSize'], 
               config['DEFAULT']['EmbeddingDim'], 
               input_length=config['DEFAULT']['MaxLength']),
-    tf.keras.layers.Conv1D(64, 5), #linear activation
-    tf.keras.layers.BatchNormalization(), #BaN for performance
-    tf.keras.layers.Activation(activation='relu'),   #Activation after BaN
+    tf.keras.layers.Conv1D(64, 5),                          # linear activation
+    tf.keras.layers.BatchNormalization(),                   # BaN for performance
+    tf.keras.layers.Activation(activation='relu'),          # Activation after BaN
     tf.keras.layers.MaxPooling1D(pool_size=4),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.LSTM(64),
-    tf.keras.layers.BatchNormalization(), #BaN for performance
+    tf.keras.layers.BatchNormalization(),                   # BaN for performance
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(config['DEFAULT']['VocabSize'], activation='softmax')
     ])
+
+
 model.compile(loss='sparse_categorical_crossentropy', 
           optimizer='adam', 
-          metrics=['accuracy'])
+          metrics=['accuracy', 'precision', 'recall', 'f1_score',perplexity]) 
 
 early_stopping = tf.keras.callbacks.EarlyStopping(
           monitor='val_loss',
@@ -52,9 +61,30 @@ early_stopping = tf.keras.callbacks.EarlyStopping(
           restore_best_weights=True
           )
 
-history = model.fit(
+# Learning phase
+
+history = model.fit(                        # Using variable history to evalute model in the future
           training_data, training_labels, 
-          epochs=100, 
-          verbose=2,
+          epochs=10000,                     # Default high value controlled by early stopping
+          verbose=2,                        # Using whole log information
+          validation_split=0.15,            # 15% of validation data because of using potentially small dataset
           callbacks=[early_stopping]
           )
+
+
+# Evaluation phase
+
+output_directory = 'output_plots'
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+default_metrics = ['accuracy', 'precision', 'recall', 'f1_score']
+
+for metric_name in default_metrics:
+    plot_and_save_metric(history, metric_name, output_directory)    # Will create plots for all metrics
+
+model_output_directory = 'output_model'
+if not os.path.exists(model_output_directory):
+    os.makedirs(model_output_directory)
+
+model.save("output_model/output_model.h5") # saving model in h5 format TODO: format is debatable
--- tools.py
+++ tools.py
@@ -1,15 +1,36 @@
 import json
+import string
+import os
+
 import numpy
+import nltk
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 def load_data(data_path):
-    with open(data_path, 'r', encoding='utf-8') as f:
-    # read RAW data from dataset given
-    pass
-    # raw_data = f.read()
-    # return raw_data
+    ''' Function that loads raw data from JSON file path.
+        UTF-8 coding required.
+    '''
+    try:
+        with open(data_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"File '{data_path}' not found.")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Decoding problem with JSON: {e} . Check if your file is coded UTF-8.")
+        return None
 
 # Preprocess data
 def preprocess(data):
+    '''Function used to preprocess raw data text.
+       Tokenization, removing punctuation, 
+       lowering cases and checks for stopwords in English.
+    '''
     # Tokenize data
     tokens = nltk.word_tokenize(data)
     # Lowercase all words
@@ -25,3 +46,38 @@ def preprocess(data):
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
     
     return tokens
+
+def perplexity(y_true, y_pred):
+    # Perplexity metric module for better LLM evaluation
+    cross_entropy = tf.keras.backend.sparse_categorical_crossentropy(y_true, y_pred)
+    perplexity = tf.exp(tf.reduce_mean(cross_entropy))
+
+    return perplexity
+
+# Metrics visualisation.
+def plot_and_save_metric(history, metric_name, output_dir):
+    ''' Function that creates visualisation of evaluation of model based on learning history.
+        history - history form keras object,
+        metric_name - name of metric form tensorflow keras documentation,
+        output_dir - string type path for output files
+    '''
+    # Pobierz liczbę epok z historii treningu
+    epochs = range(1, len(history.history[metric_name]) + 1)
+
+    metric_values = history.history[metric_name]
+    val_metric_name = 'val_' + metric_name
+    val_metric_values = history.history[val_metric_name]
+
+    # Tworzenie wykresu
+    plt.figure()
+    plt.plot(epochs, metric_values, 'b', label='Training ' + metric_name)
+    plt.plot(epochs, val_metric_values, 'r', label='Validation ' + metric_name)
+    plt.title('Training and Validation ' + metric_name)
+    plt.xlabel('Epochs')
+    plt.ylabel(metric_name)
+    plt.legend()
+
+    # Zapisywanie wykresu jako obraz
+    output_path = os.path.join(output_dir, metric_name + '_plot.png')
+    plt.savefig(output_path)
+    plt.close()
