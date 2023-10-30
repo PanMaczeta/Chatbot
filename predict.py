db818d7cb2cc99cc94da064c71df3ad9b5656ed3
PanMaczeta <52625108+PanMaczeta@users.noreply.github.com>
2023-10-30 14:34:37 +0100

    Modified comments

--- predict.py
+++ predict.py
@@ -3,14 +3,16 @@ import numpy as np
 from tools import load_data
 import tensorflow as tf
 
-model = tf.keras.models.load_model("output_model.h5")  # Podaj ścieżkę do zapisanego modelu
+model = tf.keras.models.load_model("output_model.h5")  # Input path to saved model
 
 # Prepare input data
 test_data = np.random.rand(10, 64)  # example data
 
-# Wykonaj inferencję na danych testowych
+# Do prediction
 predictions = model.predict(test_data)
 
 # Show predictions
 print("Predictions:")
 print(predictions)
+
+# TODO: Add UI or communication for best live predictions
